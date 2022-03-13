759483363ff3ea662a3425ff968dfd8de9203760
Team-1493 <acastagna@albany.k12.ny.us>
2022-03-13 12:20:01 -0400

    Changed bump to specifying omega

--- src/main/java/frc/robot/commands/Rotate/HeadingBumpCW.java
+++ src/main/java/frc/robot/commands/Rotate/HeadingBumpCW.java
@@ -1,11 +1,14 @@
 package frc.robot.commands.Rotate;
 import frc.robot.subsystems.SwerveDriveSystem;
-
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class HeadingBumpCW extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final SwerveDriveSystem m_swervedrive;
+  double initialHeading=0;
+  double finalHeading=0;
+  Timer timer = new Timer();
  
 
   public HeadingBumpCW(SwerveDriveSystem swervedrive) {
@@ -14,12 +17,19 @@ public class HeadingBumpCW extends CommandBase {
   }
 
   @Override
-  public void initialize() {}
+  public void initialize() {
+    initialHeading=m_swervedrive.heading;
+    finalHeading=initialHeading-0.026;  // 1.5 degrees
+    
+    timer.start();
+    timer.reset();
+  }
 
   @Override
   public void execute() {
     // supply vx,vy,heading setpoint
-    m_swervedrive.headingBumpCW();
+//    m_swervedrive.headingBumpCW();
+    m_swervedrive.setMotors(new double[] {0,0,-0.3,1});
   }
 
   
@@ -34,7 +44,7 @@ public class HeadingBumpCW extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return (timer.hasElapsed(.2) || m_swervedrive.heading<=finalHeading);
   }
   
 }
